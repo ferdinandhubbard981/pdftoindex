5d97914599ad89f799ab03f7250ff3f8bf012dd6
Ferdinand Hubbard <ferdinandhubbar@gmail.com>
2023-05-14 13:07:43 +0100

    added argparse

--- src/main.py
+++ src/main.py
@@ -1,30 +1,32 @@
-import sys
 import os
 import PyPDF2
 import reportlab
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from collections import defaultdict
+import argparse
+
 
 def extract_words(pdf_path, blacklist):
     # Open the PDF file
     with open(pdf_path, 'rb') as file:
         reader = PyPDF2.PdfFileReader(file)
-        
+
         # Create a dictionary to store words and their page numbers
         word_pages = defaultdict(set)
-        
+
         # Go through each page
         for page_num in range(reader.getNumPages()):
             page = reader.getPage(page_num)
             words = page.extractText().split()
-            
+
             # Store each word and its page number
             for word in words:
                 if word not in blacklist:
                     word_pages[word].add(page_num + 1)  # add 1 to start pages at 1
 
     return word_pages
+
 
 def create_index_pdf(word_pages, dir_path):
     # Create the index.pdf file
@@ -55,7 +57,12 @@ def create_index_pdf(word_pages, dir_path):
     c.drawText(textobject)
     c.save()
 
-def main(pdf_path, blacklist_path, dir_path):
+
+def main(args):
+    pdf_path = args.pdf
+    blacklist_path = args.blacklist
+    dir_path = args.dir
+
     # Read the blacklist words
     with open(blacklist_path, 'r') as file:
         blacklist = set(line.strip() for line in file)
@@ -66,9 +73,12 @@ def main(pdf_path, blacklist_path, dir_path):
     # Create the index.pdf file
     create_index_pdf(word_pages, dir_path)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print(f"Usage: {sys.argv[0]} PDF_PATH BLACKLIST_PATH DIR_PATH")
-        sys.exit(1)
 
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process a PDF file and create an index PDF.")
+    parser.add_argument("--pdf", help="Path to the PDF file")
+    parser.add_argument("--blacklist", help="Path to the blacklist file")
+    parser.add_argument("--dir", help="Path to the output directory")
+    args = parser.parse_args()
+
+    main(args)
